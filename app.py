935e842ba0d7a675cb3e6d1774556e96c87571c8
Jesus Lara <jelara99@gmail.com>
2022-10-12 14:39:00 -0600

    pruebas de funcionamiento

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask, jsonify
 
 app  = Flask(__name__)
 
+productos = [
+    {"producto":"laptop","precio":800,"cantidad":4},
+    {"producto":"mouse","precio":40,"cantidad":10},
+    {"producto":"monitor","precio":500,"cantidad":2}
+]
+
+@app.route('/productos', methods=['GET'])
+def datos():
+    return jsonify({"productos":productos}) 
+
+@app.route('/productos/<string:nombre>', methods=['GET'])
+def getProduct(nombre):
+    for salida in productos:
+        if salida["producto"] == nombre:
+            return jsonify({"producto":salida}) 
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
